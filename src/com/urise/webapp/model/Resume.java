aa59609806a4e7b068bd5f81e59c87aa39b16cae
Aleksey Kaminskiy <kaminskiy.aleksey.rus@gmail.com>
2021-09-29 12:55:58 +0300

    lesson 02 #2

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,10 +1,20 @@
+package com.urise.webapp.model;
+
 /**
  * Initial resume class
  */
 public class Resume {
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
     // Unique identifier
-    String uuid;
+    private String uuid;
 
     @Override
     public String toString() {
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,54 +1,97 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private final int RESUME_MAX_SIZE = 4; //10000;
+    private Resume[] storage = new Resume[RESUME_MAX_SIZE];
+    private int size = 0;
 
-    void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+    public void clear() {
+        Arrays.fill(storage,0,size - 1, null);
         size = 0;
     }
 
-    void save(Resume r) {
-        storage[size] = r;
-        size++;
-    }
-
-    Resume get(String uuid) {
+    /* проверка на наличие резюме в storage
+    если нашли, то возвращаем порядковый номер,
+    если не нашли, то возвращаем -1
+     */
+    public int check(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                return storage[i];
+            if (storage[i].getUuid() == uuid) {
+                return i;
             }
         }
-        return null;
+        return -1;
     }
 
-    void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                break;
-            }
+
+    public void update(Resume r) {
+        int i = check(r.getUuid());
+        if (i != -1){
+            System.out.println("Обновляем " + storage[i].getUuid());
+            //Здесь должна быть логика обновления storage[i]
+            //Сейчас обновлять нечего, т.к. у нас только одно ключевое поле в классе Resume
+        }
+        else{
+            System.out.println("Ошибка update. Резюме не найдено");
+        }
+    }
+
+    public void save(Resume r) {
+        if (size == RESUME_MAX_SIZE){
+            System.out.println("Ошибка save. Переполнение массива резюме");
+            return;
+        }
+        if (check(r.getUuid()) == -1){
+           storage[size] = r;
+           size++;
+        }
+        else{
+            System.out.println("Ошибка save. Резюме уже есть");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int i = check(uuid);
+        if (i != -1){
+            return storage[i];
+        }
+        else{
+            System.out.println("Ошибка get. Резюме не найдено");
+            return null;
+        }
+    }
+
+    public void delete(String uuid) {
+        int i = check(uuid);
+        if (i != -1){
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+        else{
+            System.out.println("Ошибка delete. Резюме не найдено");
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] allResume = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            allResume[i] = storage[i];
-        }
+
+        allResume = Arrays.copyOf(storage, size);
+
         return allResume;
     }
 
-    int size() {
+     public int size() {
         return size;
     }
 }
