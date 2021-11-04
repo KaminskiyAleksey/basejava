c524506c345e173bba2c88ea9bfe82fe207af40e
Aleksey Kaminskiy <kaminskiy.aleksey.rus@gmail.com>
2021-11-04 16:43:05 +0300

    lesson 10 #2

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -1,12 +1,16 @@
 package com.urise.webapp.storage.serializer;
 
-import com.urise.webapp.model.ContactType;
-import com.urise.webapp.model.Resume;
+import com.urise.webapp.model.*;
+import com.urise.webapp.storage.AbstractStorage;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class DataStreamSerializer implements StreamSerializer {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
@@ -20,6 +24,12 @@ public class DataStreamSerializer implements StreamSerializer {
                 dos.writeUTF(entry.getValue());
             }
             // TODO implements sections
+            Map<SectionType, Section> sections = r.getSections();
+            dos.writeInt(sections.size());
+            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                writeSection(entry.getKey(), entry.getValue(), dos);
+            }
         }
     }
 
@@ -34,7 +44,110 @@ public class DataStreamSerializer implements StreamSerializer {
                 resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
             // TODO implements sections
+            int sizeSection = dis.readInt();
+            Section section;
+            for (int i = 0; i < sizeSection; i++) {
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                section = readSection(sectionType, dis);
+                resume.addSection(sectionType,  section);
+            }
             return resume;
         }
+    }
+
+    private void writeSection(SectionType sectionName, Section section, DataOutputStream dos) throws IOException {
+        LOG.info("write Section = " + sectionName);
+        switch (sectionName) {
+            case PERSONAL:
+            case OBJECTIVE:
+                dos.writeUTF(((TextSection) section).getContent());
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                List<String> items = ((ListSection) section).getItems();
+                dos.write(items.size());
+                LOG.info("size = " + items.size());
+                for (String str : ((ListSection) section).getItems()) {
+                    dos.writeUTF(str);
+                }
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                List<Organization> organizationItems = ((OrganizationSection) section).getOrganizations();
+                dos.write(organizationItems.size());
+                LOG.info("size = " + organizationItems.size());
+                for (Organization organization : ((OrganizationSection) section).getOrganizations()) {
+                    writeOrganization(organization, dos);
+                }
+                break;
+        }
+    }
+
+    private void writeOrganization(Organization organization , DataOutputStream dos) throws IOException {
+        dos.writeUTF(organization.getHomePage().getName());
+        dos.writeUTF(organization.getHomePage().getUrl());
+        List<Organization.Position> positions = organization.getPositions();
+        dos.write(positions.size());
+        for (Organization.Position position : organization.getPositions()) {
+            writePosition(position, dos);
+        }
+    }
+
+    private void writePosition(Organization.Position position , DataOutputStream dos) throws IOException {
+        dos.writeUTF(position.getStartDate().toString());
+        dos.writeUTF(position.getEndDate().toString());
+        dos.writeUTF(position.getTitle());
+        dos.writeUTF(position.getDescription());
+    }
+
+    private Section readSection(SectionType sectionName, DataInputStream dis) throws IOException {
+        Section section = null;
+        int sectionSize;
+        LOG.info("read Section = " + sectionName);
+        switch (sectionName) {
+            case PERSONAL:
+            case OBJECTIVE:
+                section = new TextSection(dis.readUTF());
+                LOG.info("Text Section = " + section);
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                List<String> listItems = null;
+                sectionSize = dis.readInt();
+                LOG.info("size = " + sectionSize);
+                for (int i = 0; i < sectionSize; i++) {
+                    listItems.add(dis.readUTF());
+                }
+                section = new ListSection(listItems);
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                List<Organization> organizationItems = null;
+                sectionSize = dis.readInt();
+                LOG.info("size = " + sectionSize);
+                for (int i = 0; i < sectionSize; i++) {
+                    organizationItems.add(readOrganization(dis));
+                }
+                section = new OrganizationSection(organizationItems);
+                break;
+        }
+        return section;
+    }
+
+    private Organization readOrganization(DataInputStream dis) throws IOException {
+        List<Organization.Position> positions = null;
+
+        Link link = new Link(dis.readUTF(), dis.readUTF());
+        int positionSize = dis.readInt();
+
+        for (int i = 0; i < positionSize; i++) {
+            positions.add(readPosition(dis));
+        }
+        Organization organization = new Organization(link,  positions);
+        return organization;
+    }
+
+    private Organization.Position readPosition(DataInputStream dis) throws IOException {
+        return new Organization.Position(LocalDate.parse(dis.readUTF()),LocalDate.parse(dis.readUTF()),dis.readUTF(),dis.readUTF());
     }
 }
--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -37,7 +37,7 @@ public abstract class AbstractStorageTest {
 
         R1.addContact(ContactType.MAIL, "mail1@ya.ru");
         R1.addContact(ContactType.PHONE, "11111");
-        /*R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
+        R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
         R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
         R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achivment11", "Achivment12", "Achivment13"));
         R1.addSection(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
@@ -48,10 +48,10 @@ public abstract class AbstractStorageTest {
                                 new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
         R1.addSection(SectionType.EDUCATION,
                 new OrganizationSection(
-                        new Organization("Institute", null,
-                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
+                        new Organization("Institute", "http://null.ru",
+                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", "null description"),
                                 new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")),
-                        new Organization("Organization12", "http://Organization12.ru")));*/
+                        new Organization("Organization12", "http://Organization12.ru")));
         R2.addContact(ContactType.SKYPE, "skype2");
         R2.addContact(ContactType.PHONE, "22222");
         /*R1.addSection(SectionType.EXPERIENCE,
