899d4cacd098fbb9c95fcf22c23bb9bb9ef9c4b8
Aleksey Kaminskiy <kaminskiy.aleksey.rus@gmail.com>
2021-11-07 22:19:09 +0300

    lesson 10 #5

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -7,45 +7,75 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
-import static com.urise.webapp.util.StringUtil.*;
+import static com.urise.webapp.util.StringUtil.convertToEmptyIfNull;
+import static com.urise.webapp.util.StringUtil.convertToNullIfEmpty;
 
 public class DataStreamSerializer implements StreamSerializer {
     @FunctionalInterface
-    interface FunctionalElement<T> {
+    interface FunctionalElementWriter<T> {
         void write(T t) throws IOException;
     }
 
+    private interface FunctionalElementReadMap {
+        void process() throws IOException;
+    }
+
+    @FunctionalInterface
+    interface FunctionalElementReadList<T> {
+        T read() throws IOException;
+    }
+
+    public static void WriteDate(DataOutputStream dos, LocalDate date) throws IOException {
+        dos.writeInt(date.getYear());
+        dos.writeInt(date.getMonthValue());
+    }
+    
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
-            Map<ContactType, String> contacts = r.getContacts();
-            writeWithException(dos, contacts.entrySet(), entry -> {
+            writeWithException(dos, r.getContacts().entrySet(), entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             });
-            /*dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }*/
 
-            Map<SectionType, Section> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                writeSection(entry.getKey(), entry.getValue(), dos);
-            }
+            writeWithException(dos, r.getSections().entrySet(), entry -> {
+                SectionType sectionName = entry.getKey();
+                Section section = entry.getValue();
+                dos.writeUTF(sectionName.name());
+                switch (sectionName) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        dos.writeUTF(((TextSection) section).getContent());
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        writeWithException(dos, ((ListSection) section).getItems(), dos::writeUTF);
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        writeWithException(dos, ((OrganizationSection) section).getOrganizations(), org -> {
+                            dos.writeUTF(convertToEmptyIfNull(org.getHomePage().getName()));
+                            dos.writeUTF(convertToEmptyIfNull(org.getHomePage().getUrl()));
+                            writeWithException(dos, org.getPositions(), pos -> {
+                                WriteDate(dos, pos.getStartDate());
+                                WriteDate(dos, pos.getEndDate());
+                                dos.writeUTF(pos.getTitle());
+                                dos.writeUTF(convertToEmptyIfNull(pos.getDescription()));
+                            });
+                        });
+                        break;
+                }
+            });
         }
     }
 
-    private <T> void writeWithException (DataOutputStream dos, Collection<T> collection, FunctionalElement<T> functionalElement) throws IOException {
+    private <T> void writeWithException(DataOutputStream dos, Collection<T> collection, FunctionalElementWriter<T> functionalElementWriter) throws IOException {
         dos.writeInt(collection.size());
         for (T item : collection) {
-            functionalElement.write(item);
+            functionalElementWriter.write(item);
         }
     }
 
@@ -55,117 +85,51 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
+            readMapWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
 
-            int sizeSection = dis.readInt();
-            Section section;
-            for (int i = 0; i < sizeSection; i++) {
+            readMapWithException(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
-                section = readSection(sectionType, dis);
-                resume.addSection(sectionType, section);
-            }
+                resume.addSection(sectionType, readSectionWithException(dis, sectionType));
+            });
             return resume;
         }
     }
 
-    private void writeSection(SectionType sectionName, Section section, DataOutputStream dos) throws IOException {
-        switch (sectionName) {
-            case PERSONAL:
-            case OBJECTIVE:
-                dos.writeUTF(((TextSection) section).getContent());
-                break;
-            case ACHIEVEMENT:
-            case QUALIFICATIONS:
-                List<String> items = ((ListSection) section).getItems();
-                dos.writeInt(items.size());
-                for (String str : ((ListSection) section).getItems()) {
-                    dos.writeUTF(str);
-                }
-                break;
-            case EXPERIENCE:
-            case EDUCATION:
-                List<Organization> organizationItems = ((OrganizationSection) section).getOrganizations();
-                dos.writeInt(organizationItems.size());
-                for (Organization organization : ((OrganizationSection) section).getOrganizations()) {
-                    writeOrganization(organization, dos);
-                }
-                break;
+    private <T> void readMapWithException(DataInputStream dis, FunctionalElementReadMap reader) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            reader.process();
         }
     }
 
-    private void writeOrganization(Organization organization, DataOutputStream dos) throws IOException {
-        dos.writeUTF(convertToEmptyIfNull(organization.getHomePage().getName()));
-        dos.writeUTF(convertToEmptyIfNull(organization.getHomePage().getUrl()));
-        List<Organization.Position> positions = organization.getPositions();
-        dos.writeInt(positions.size());
-        for (Organization.Position position : organization.getPositions()) {
-            writePosition(position, dos);
-        }
-    }
-
-    private void writePosition(Organization.Position position, DataOutputStream dos) throws IOException {
-        dos.writeInt(position.getStartDate().getYear());
-        dos.writeInt(position.getStartDate().getMonthValue());
-        dos.writeInt(position.getEndDate().getYear());
-        dos.writeInt(position.getEndDate().getMonthValue());
-        dos.writeUTF(position.getTitle());
-        dos.writeUTF(convertToEmptyIfNull(position.getDescription()));
-    }
-
-    private Section readSection(SectionType sectionName, DataInputStream dis) throws IOException {
-        int sectionSize;
+    private Section readSectionWithException(DataInputStream dis, SectionType sectionName) throws IOException {
         switch (sectionName) {
             case PERSONAL:
             case OBJECTIVE:
                 return new TextSection(dis.readUTF());
             case ACHIEVEMENT:
             case QUALIFICATIONS:
-                List<String> listItems = new ArrayList<>();
-                sectionSize = dis.readInt();
-                for (int i = 0; i < sectionSize; i++) {
-                    listItems.add(dis.readUTF());
-                }
-                return new ListSection(listItems);
+                return new ListSection(readListWithException(dis, dis::readUTF));
             case EXPERIENCE:
             case EDUCATION:
-                List<Organization> organizationItems = new ArrayList<>();
-                sectionSize = dis.readInt();
-                for (int i = 0; i < sectionSize; i++) {
-                    organizationItems.add(readOrganization(dis));
-                }
-                return new OrganizationSection(organizationItems);
+                return new OrganizationSection(
+                        readListWithException(dis, () -> new Organization(
+                                new Link(convertToNullIfEmpty(dis.readUTF()), convertToNullIfEmpty(dis.readUTF())),
+                                readListWithException(dis, () -> new Organization.Position(
+                                        LocalDate.of(dis.readInt(), dis.readInt(), 1), LocalDate.of(dis.readInt(), dis.readInt(), 1), dis.readUTF(), convertToNullIfEmpty(dis.readUTF())
+                                ))
+                        )));
             default:
                 throw new IllegalStateException();
         }
     }
 
-    private Organization readOrganization(DataInputStream dis) throws IOException {
-        List<Organization.Position> positions = new ArrayList<>();
-        String name = convertToNullIfEmpty(dis.readUTF());
-        String url = convertToNullIfEmpty(dis.readUTF());
-
-        Link link = new Link(name, url);
-        int positionSize = dis.readInt();
-
-        for (int i = 0; i < positionSize; i++) {
-            positions.add(readPosition(dis));
+    private <T> List<T> readListWithException(DataInputStream dis, FunctionalElementReadList<T> reader) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(reader.read());
         }
-        Organization organization = new Organization(link, positions);
-        return organization;
-    }
-
-    private Organization.Position readPosition(DataInputStream dis) throws IOException {
-        int dateYear = dis.readInt();
-        int dateMonth = dis.readInt();
-        LocalDate startDate = LocalDate.of(dateYear,dateMonth,1);
-        dateYear = dis.readInt();
-        dateMonth = dis.readInt();
-        LocalDate endDate = LocalDate.of(dateYear,dateMonth,1);
-        String title = dis.readUTF();
-        String description = convertToNullIfEmpty(dis.readUTF());
-        return new Organization.Position(startDate, endDate, title, description);
+        return list;
     }
 }
